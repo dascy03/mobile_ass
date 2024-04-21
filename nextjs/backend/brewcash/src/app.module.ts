5cb5cfc43f9bd09cde655da68d2fd9d55b3d2294
Clay <tadinhtien5@gmail.com>
2024-04-21 19:16:47 +0700

    init user module, wallet module

--- nextjs/backend/brewcash/src/app.module.ts
+++ nextjs/backend/brewcash/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { WalletsModule } from './wallets/wallets.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://clay:1234@brewcash.flk27ma.mongodb.net/brewcash?retryWrites=true&w=majority&appName=BrewCash',
+    ),
+    UsersModule,
+    WalletsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
