7689400f10fc0a86ae119af25ee688e1e136d026
Thái Bảo Long <subin27102003@gmail.com>
2024-05-22 23:48:45 +0700

    fix nextjs

--- nextjs/backend/brewcash/src/transactions/transactions.controller.ts
+++ nextjs/backend/brewcash/src/transactions/transactions.controller.ts
@@ -16,80 +16,80 @@ import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('transactions')
 export class TransactionsController {
-  constructor(private transactionsService: TransactionsService) {}
+  constructor(private transactionsService: TransactionsService) {
+  }
 
   @Post()
-  @ApiResponse({status:200, description:"successfully"})
-  @ApiResponse({status:500, description:"fail!"})
+  @ApiResponse({status: 200, description: "successfully"})
+  @ApiResponse({status: 500, description: "fail!"})
   async create(
-    @Body() createTransactionDto: CreateTransactionDto,
+      @Body() createTransactionDto: CreateTransactionDto,
   ): Promise<Object> {
     try {
       return await this.transactionsService.create(createTransactionDto);
     } catch (err) {
-      return { message: err.message || 'Internal Server Error' };
+      return {message: err.message || 'Internal Server Error'};
     }
   }
+
   @Get()
-  @ApiResponse({status:200, description:"successfully"})
-  @ApiResponse({status:500, description:"fail!"})
+  @ApiResponse({status: 200, description: "successfully"})
+  @ApiResponse({status: 500, description: "fail!"})
   async findAll(): Promise<Object> {
     try {
       return await this.transactionsService.findAll();
     } catch (err) {
-      return { message: err.message || 'Internal Server Error' };
+      return {message: err.message || 'Internal Server Error'};
     }
   }
 
 
   @Get(':category')
-  @ApiResponse({status:200, description:"successfully"})
-    @ApiResponse({status:500, description:"fail!"})
+  @ApiResponse({status: 200, description: "successfully"})
+  @ApiResponse({status: 500, description: "fail!"})
 
   async findByCategory(@Param('category') category: string): Promise<Object> {
     try {
       return await this.transactionsService.findByCategory(category);
     } catch (err) {
-      return { message: err.message || 'Internal Server Error' };
+      return {message: err.message || 'Internal Server Error'};
     }
   }
 
 
   @Get(':date')
-  @ApiResponse({status:200, description:"successfully"})
-    @ApiResponse({status:500, description:"fail!"})
-  async findByDate(@Param('date') date: Date): Promise<Object> {
+  @ApiResponse({status: 200, description: "successfully"})
+  @ApiResponse({status: 500, description: "fail!"})
 
   async findByDate(@Param('date') date: string): Promise<Object> {
-
     try {
       return await this.transactionsService.findByDate(date);
     } catch (err) {
-      return { message: err.message || 'Internal Server Error' };
+      return {message: err.message || 'Internal Server Error'};
     }
   }
 
   @Patch()
-  @ApiResponse({status:200, description:"successfully"})
-    @ApiResponse({status:500, description:"fail!"})
+  @ApiResponse({status: 200, description: "successfully"})
+  @ApiResponse({status: 500, description: "fail!"})
   async update(
-    @Body() updateTransactionDto: UpdateTransactionDto,
+      @Body() updateTransactionDto: UpdateTransactionDto,
   ): Promise<Object> {
     try {
       return await this.transactionsService.update(updateTransactionDto);
     } catch (err) {
-      return { message: err.message || 'Internal Server Error' };
+      return {message: err.message || 'Internal Server Error'};
     }
   }
 
   @Delete(':id')
-  @ApiResponse({status:200, description:"successfully"})
-    @ApiResponse({status:500, description:"fail!"})
+  @ApiResponse({status: 200, description: "successfully"})
+  @ApiResponse({status: 500, description: "fail!"})
   async remove(@Param('id') id: string): Promise<Object> {
     try {
       return await this.transactionsService.remove(+id);
     } catch (err) {
-      return { message: err.message || 'Internal Server Error' };
+      return {message: err.message || 'Internal Server Error'};
     }
   }
 }
